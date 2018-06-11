de73e35a3ac545825e7794761f34af009545eec6
moorej3 <34171989+moorej3@users.noreply.github.com>
2018-06-11 14:33:06 -0700

    Add files via upload

---
+++ IFTControlModule.py
@@ -0,0 +1,19 @@
+from os import walk
+import IftAnalysis
+
+def listfiles(directory, extension):
+    filelist = []
+    for(dirpath, dirnames, filenames) in walk(directory):
+        for f in filenames:
+             if f.endswith(extension):
+                 #print(dirpath+"/"+ f)
+                 filelist.append(dirpath+"/" + f)
+    return(filelist)
+
+a = listfiles("./", "filtered_forward.tif")
+
+print(a)
+
+for file in a:
+    filename = file.split("./")[1].split("/")[0]
+    IftAnalysis.IFTTraj(file, filename)
